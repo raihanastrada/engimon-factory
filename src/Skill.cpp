939cfbdde9f2e7401c72345750944befae8d17de
Jauhar Wibisono <jauhar.wibisono@gmail.com>
2021-03-23 10:29:36 +0700

    add compatible elements ke skill, assuming boleh pake stl

--- src/Skill.cpp
+++ src/Skill.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 Skill::Skill() : base_power(0), mastery_level(0) {}
 
-Skill::Skill(int _base_power, int _mastery_level) : base_power(_base_power), mastery_level(_mastery_level) {}
+Skill::Skill(int _base_power, int _mastery_level, vector<Element> _compatible_Element){
+	this->base_power = _base_power;
+	this->mastery_level = _mastery_level;
+	this->compatible_Element = _compatible_Element; // vector punya assignment op
+}
 
 int Skill::getBasePower() const{
 	return base_power;
@@ -14,10 +18,18 @@ int Skill::getMasteryLevel() const{
 	return mastery_level;
 }
 
+vector<Element> getCompatibleElement() const{
+	return compatible_Element;
+}
+
 void Skill::setBasePower(int _base_power){
 	base_power = _base_power;
 }
 
 void Skill::setMasteryLevel(int _mastery_level){
 	mastery_level = _mastery_level;
+}
+
+void setCompatibleElement(vector<Element> _compatible_Element){
+	this->compatible_Element = _compatible_Element;
 }
--- src/Skill.h
+++ src/Skill.h
@@ -4,25 +4,31 @@
 #define __SKILL_H
 
 #include <stdio.h>
+#include <vector>
+#include <iostream>
+#include "Elements.hpp"
 using namespace std;
 
 class Skill{
 private:
 	int base_power;
 	int mastery_level;
+	vector<Element> compatible_Element; // diasumsikan boleh pake STL di class skill
 public:
 	// default ctor, rasanya nggak bakal dipakai
 	Skill();
 	// custom ctor
-	Skill(int _base_power, int _mastery_level);
+	Skill(int _base_power, int _mastery_level, vector<Element> _compatible_Element);
 
 	// getter
 	int getBasePower() const;
 	int getMasteryLevel() const;
+	vector<Element> getCompatibleElement() const;
 
 	// setter
 	void setBasePower(int _base_power);
 	void setMasteryLevel(int _mastery_level);
+	void setCompatibleElement(vector<Element> _compatible_Element);
 };
 
 #endif
