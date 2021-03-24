b314c7f7123d315920fa81710cd302b9ed770b56
ayahyaaa <68509074+ayahyaaa@users.noreply.github.com>
2021-03-24 23:34:50 +0700

    Update Breed.cpp
    pemutakhiran, still no fixed engimon though ;((

--- src/Breed.cpp
+++ src/Breed.cpp
@@ -56,7 +56,7 @@ Engimon Engimon::breed(Engimon dad, Engimon mom) {
 
 	// set seluruh skill anak = skill bapak
 	for (int i=1;i<4;i++) {
-		if (elemen kompatibel && dad.skills[i]!=null) {
+		if (elemen kompatibel && dad.skills[i].getId() != -1) {
 			anak.skills[current_skill] = dad.skills[i];
 			current_skill ++;
 			slotted_skill ++;
@@ -65,13 +65,13 @@ Engimon Engimon::breed(Engimon dad, Engimon mom) {
 	
 	// Cek skill bapak = skill ibu
 	current_skill = 1;
-	while(current_skill<slotted_skill && anak.skills[current_skill]!=null) {
+	while(current_skill<slotted_skill) {
 		for (int i=1;i<4;i++) {
-			if (anak.skills[current_skill] == mom.skills[i] && 
-						anak.skills[current_skill].getMasteryLevel < mom.skills[i].getMasteryLevel()) {
+			if (anak.skills[current_skill].getId() == mom.skills[i].getId() && 
+						anak.skills[current_skill] < mom.skills[i]) {
 				anak.skills[current_skill].setMasteryLevel(mom.skills[i].getMasteryLevel());
 			}
-			else if (anak.skills[current_skill] == mom.skills[i] && 
+			else if (anak.skills[current_skill].getId() == mom.skills[i].getId() && 
 						anak.skills[current_skill].getMasteryLevel == mom.skills[i].getMasteryLevel()) {
 				anak.skills[current_skill].setMasteryLevel(mom.skills[i].getMasteryLevel()+1);
 			}
@@ -79,26 +79,38 @@ Engimon Engimon::breed(Engimon dad, Engimon mom) {
 		current_skill++;
 	}
 	
-	// Cek mastery level skill
-	current_skill = 1;
-	Skill* temp = new Skill();
-	int temp_idx = 0;
-	while(current_skill<slotted_skill && anak.skills[current_skill!=null]) {
-		for (int i=1;i<4;i++) {
-			if (elemen kompatibel && mom.skills[i]!=null) {
-				if (anak.skills[current_skill].getMasteryLevel > mom.skills[i].getMasteryLevel()) {
-					temp[temp_idx] = anak.skills[current_skill];
-					temp_idx ++;
-					anak.skills[current_skill] = mom.skills[i];
-				}
+	// Cek mastery level skill, ganti dengan skill ibu kalau lebih besar
+	vector<Skill> temp;
+	for (int i=1;i<4;i++) {
+		current_skill = 1;
+		while (current_skill<slotted_skill && mom.skills[i].getId() != -1) {
+			if (anak.skills[current_skill] < mom.skills[i]) {
+				temp.push_back(anak.skills[current_skill]);
+				anak.skills[current_skill] = mom.skills[i];
+				break;
 			}
+			current_skill++;
 		}
-		current_skill++;
 	}
 	
-	// maksimalisasi jumlah skill
-	temp.sort();
-	temp_idx = 0;
+	// Cek mastery level skill, ganti dengan skill yang sudah terbuang kalau lebih besar
+	for (int i=1;i<slotted_skill;i++) {
+		for (int j=1;j<temp_size;j++) {
+			if (anak.skills[i] < temp[j]) {
+				Skill swap();
+				swap = anak.skills[i];
+				anak.skills[i] = temp[i];
+				temp[i] = swap;
+				break;
+			}
+		}
+	}
+	
+	// sort skill yang kebuang berdasarkan mastery level
+	sort(temp.rbegin(),temp.rend());
+	
+	// maxing skill amount	
+	int temp_idx = 0;
 	while(slotted_skill<4) {
 		current_skill = slotted_skill;
 		anak.skills[current_skill] = temp[temp_idx];
