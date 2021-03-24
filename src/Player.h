4c380a9324f3a1cc49d173d84fe3e1d99e14d5e1
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2021-03-24 23:13:47 +0700

    update Player, Point, inisialisasi main

--- src/Player.h
+++ src/Player.h
@@ -3,8 +3,10 @@
 
 #include <iostream>
 #include <string>
+#include "Inventory.hpp"
 #include "Point.h"
 #include "Engimon.hpp"
+#include "Skill.h"
 
 using namespace std;
 
@@ -13,29 +15,34 @@ class Player {
         string PlayerName;
         Point EngimonLocation; // Previous Location (Engimon mengikuti gerakan Player)
         Point Location; // Lokasi Player
-        // Inventory<Engimon> InvEngimon // (Kelas Engimon)
-        // Inventory<SkillItem> InvSkillItem // (Kelas Skill Item)
-        // Engimon* ActiveEngimon;
+        Inventory<Engimon> InvE; // Inventory Engimon (Kelas Engimon)
+        // Inventory<Skill> InvS; // (Kelas Skill Item)
+        Engimon* ActiveEngimon; // Pointer ke Active Engimon
         static int maxCapacity; // Kapasitas maksimum inventory
     public:
         Player(); // Default constructor
         Player(string name); // User-defined constructor
+        Player& operator=(const Player&); // Operator Assignment
         ~Player(); // Destructor
-        Point getLocation();
-        Point getEngimonLocation();
+        Point getLocation(); // Getter Location Player
+        Point getEngimonLocation(); // Getter Location Active Engimon
         void Move(char direction); // Bergerak ke kiri, kanan, atas, atau bawah
         void CheckActive(); // Mengecek active engimon
-        void SwitchActive(string EngimonName); // Mengganti active engimon
+        void SwitchActive(int index); // Mengganti active engimon // Mencari Engimon pada Inv dengan index
+        void SwitchActiveMenu(); // Menu untuk memilih mengganti Engimon Active
         void PrintListEngimon(); // Menampilkan list engimon yang dimiliki
         // Menampilkan data lengkap suatu engimon (setiap atribut kelas)
         // Juga harus menampilkan nama parent beserta spesies mereka
         void PrintListSkillItem(); // Menampilkan list skill item yang dimiliki
+        void PrintEngimonMenu(); // Print detail suatu Engimon (menampilkan nama parent beserta spesies mereka) serta seluruh atribut kelas
         void UseSkillItem(/*apaan*/); // Menggunakan skill item pada suatu engimon
         void Breeding(); // Melaksanakan breeding antara 2 engimon
-        void Battle(); // Melakukan battle dengna suatu engimon yang berada di dekatnya (adjacent tiles)
+        void Battle(); // Melakukan battle dengan suatu engimon yang berada di dekatnya (adjacent tiles)
         // Petak kiri, kanan, atas, dan bawah (tampilkan detail engimon lawan ke layar)
-        void Interact();
-        bool IsInventoryFull();
+        void Interact(); // Mengecek Active Engimon
+        void InsertEngimon(Engimon E); // Insert Engimon ke inventory
+        void InsertSkillItem(Skill S); // Insert SkillItem ke inventory
+        bool IsInventoryFull(); // Mengecek jika inventory full
 };
 
 #endif
--- src/Point.cpp
+++ src/Point.cpp
@@ -3,12 +3,21 @@
 
 using namespace std;
 
-Point::Point() : x(0), y(0) {} // Default Constructor
-Point::Point(int in_x, int in_y) : x(in_x), y(in_y) {} // User-defined Constructor (int x and int y)
-void Point::SetXY(int x, int y) { this->x = x; this->y = y; } // Setter (x and y)
-int Point::GetX() { return this->x; } // Getter x
-int Point::GetY() { return this->y; } // Getter y
-void Point::MoveUp() { this->y = y - 1; } // Bergerak ke atas (y berkurang)
-void Point::MoveDown() { this->y = y + 1; } // Bergerak ke bawah (y bertambah)
-void Point::MoveLeft() { this->x = x - 1; } // Bergerak ke kiri
-void Point::MoveRight() { this->x = x + 1; } // Bergerak ke kanan
+// Default Constructor
+Point::Point() : x(0), y(0) {}
+// User-defined Constructor (int x and int y)
+Point::Point(int in_x, int in_y) : x(in_x), y(in_y) {}
+// Setter (x and y)
+void Point::SetXY(int x, int y) { this->x = x; this->y = y; }
+// Getter Point x
+int Point::GetX() { return this->x; }
+// Getter Point y
+int Point::GetY() { return this->y; }
+// Bergerak ke atas (y berkurang)
+void Point::MoveUp() { this->y = y - 1; }
+// Bergerak ke bawah (y bertambah)
+void Point::MoveDown() { this->y = y + 1; }
+// Bergerak ke kiri
+void Point::MoveLeft() { this->x = x - 1; }
+// Bergerak ke kanan
+void Point::MoveRight() { this->x = x + 1; }
