df7364c4cebb35791867f4c01aa7c5f2b00b92ec
Jauhar Wibisono <jauhar.wibisono@gmail.com>
2021-03-24 23:09:10 +0700

    add assignment op, comparator to skill

--- src/Skill.cpp
+++ src/Skill.cpp
@@ -2,12 +2,33 @@
 #include "Skill.h"
 using namespace std;
 
-Skill::Skill() : base_power(0), mastery_level(0) {}
+Skill::Skill() : id(-1), base_power(0), mastery_level(0) {}
 
 Skill::Skill(int _base_power, int _mastery_level, vector<Element> _compatible_Element){
 	this->base_power = _base_power;
 	this->mastery_level = _mastery_level;
 	this->compatible_Element = _compatible_Element; // vector punya assignment op
+	this->id = _id;
+	curr_id++;
+}
+
+Skill::Skill(Skill &other){
+	this->base_power = other.base_power;
+	this->mastery_level = other.mastery_level;
+	this->compatible_Element = other.compatible_Element;
+	this->id = other.id;
+}
+
+Skill& Skill::operator=(Skill &other){
+	this->base_power = other.base_power;
+	this->mastery_level = other.mastery_level;
+	this->compatible_Element = other.compatible_Element;
+	this->id = other.id;
+	return this;
+}
+
+bool Skill::operator==(Skill &other){
+	return this->id == other.id;
 }
 
 int Skill::getBasePower() const{
@@ -32,4 +53,17 @@ void Skill::setMasteryLevel(int _mastery_level){
 
 void setCompatibleElement(vector<Element> _compatible_Element){
 	this->compatible_Element = _compatible_Element;
+}
+
+void printInfo(){
+	cout << "base power: " << this->base_power << "\n";
+	cout << "mastery level: " << this->mastery_level << "\n";
+	cout << "compatible element: {";
+	bool first = 1;
+	for (Element &e : compatible_Element){
+		if (first) first = 0;
+		else cout << ",";
+		cout << e.getElementName();
+	}
+	cout << "\n";
 }
--- src/Skill.h
+++ src/Skill.h
@@ -11,14 +11,23 @@ using namespace std;
 
 class Skill{
 private:
+	int id;
 	int base_power;
 	int mastery_level;
 	vector<Element> compatible_Element; // diasumsikan boleh pake STL di class skill
+	static int curr_id = 0; // id skill mulai dari nol
 public:
-	// default ctor, rasanya nggak bakal dipakai
+	// default ctor, id skill hasil default ctor -1
 	Skill();
 	// custom ctor
 	Skill(int _base_power, int _mastery_level, vector<Element> _compatible_Element);
+	// cctor
+	Skill(Skill &other);
+	// assignment op
+	Skill& operator=(Skill &other);
+
+	// comparator equal
+	bool operator==(Skill &other);
 
 	// getter
 	int getBasePower() const;
@@ -29,6 +38,9 @@ public:
 	void setBasePower(int _base_power);
 	void setMasteryLevel(int _mastery_level);
 	void setCompatibleElement(vector<Element> _compatible_Element);
+
+	// print info
+	void PrintInfo();
 };
 
 #endif
