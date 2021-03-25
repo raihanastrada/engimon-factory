cc4152ec844c934061ff377184e8cf55d200a333
Govindabhakta <igovindabhakta@gmail.com>
2021-03-25 12:28:13 +0700

    Updated Engimon

--- src/Engimon.hpp
+++ src/Engimon.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <map>
 
 #include "Point.h"
 #include "Skill.h"
@@ -13,7 +14,6 @@ using namespace std;
 
 // ENGIMON DIES AT LEVEL 100.
 #define MAX_EXP 100 * 100 
-#define Print(n) cout << n << endl
 
 /*
 ENGIDEX code format
@@ -27,6 +27,7 @@ First engimon type with elements Ground
     3001
 */
 
+// ASUMSI HANYA ADA SATU ENGIDEX UNTUK SATU GAME.
 class Engidex {
     public:
         class Species {
@@ -34,24 +35,80 @@ class Engidex {
                 string species_name;
                 int species_id;
                 Skill uniqueSkill;
+                vector<Element> elements;
             public:
-                Species();
-                Species(string nm, int id);
+                Species()
+                {
+                    species_name = "Unknown";
+                    species_id = 0;
+                }
 
-                string getSpeciesName();
-                int getEngidexID();
+                Species(int id)
+                {
+                    Species spec = Engidex::getSpecies(id);
+
+                    species_name = spec.species_name;
+                    species_id = spec.species_id;
+                    uniqueSkill = spec.uniqueSkill;
+                    elements = spec.elements;
+                };
+
+                /* Constructors for creating new species and entering into codex */
+                Species(string nm, int id, Skill u, Element e1) 
+                {
+                    species_name = nm;
+                    species_id = id;
+                    uniqueSkill = u;
+                    elements.push_back(e1);
+                };
+
+                Species(string nm, int id, Skill u, Element e1, Element e2)
+                {
+                    species_name = nm;
+                    species_id = id;
+                    uniqueSkill = u;
+                    elements.push_back(e1);
+                    elements.push_back(e2);
+                };
+
+                // Copy constructor
+                Species(const Species &spec)
+                {
+                    species_name = spec.species_name;
+                    species_id = spec.species_id;
+                    uniqueSkill = spec.uniqueSkill;
+                    elements = spec.elements;
+                }
+
+                void species_info_print();
+
+                string getSpeciesName() { return species_name; };
+                int getEngidexID() { return species_id; };
+                Skill getUniqueSkill() { return uniqueSkill; };
+
+                bool operator==( Species sp2 ) { return species_id == sp2.species_id; };
+                bool operator==( int id ) { return species_id == id; };
+                bool operator==( string name ) { return species_name == name; };
         };
 
         Engidex();
         
-        static void addSpecies(string name, Element a1, Element b2);
-        static void addSpecies(string name, Element a1);
-        static void addSpecies(string name, int firstElement, int secondElement);
-        static void addSpecies(string name, int firstElement);
-        static void getSpecies(int code);
+        static void addSpecies(string name, int id, Skill u, Element a1, Element a2)
+        {
+            Species s(name, id, u, a1, a2);
+            codex[id] = s;
+        };
+
+        static void addSpecies(string name, int id, Skill u, Element a1)
+        {
+            Species s(name, id, u, a1);
+            codex[id] = s;
+        }
+        static Species getSpecies(int code);
 
     private:
-        vector<Species> codex;
+        static map<int, Species> codex;
+        // static int generateEngidexId(); too lazy to make this
 };
 
 /*
@@ -61,24 +118,53 @@ Engimon bernama "PikaPika" dengan spesies "Pikachu" maka, PikaPika(child, engimo
 
 class Engimon : public Engidex::Species {
     private:
-        int             id;
-        string          name;
-        Engimon*        parents[2];
-        Skill           skills[4];
-        int             exp;
-        bool            alive;
+        int                 id;
+        string              name;
+        vector<Engimon*>    parents;
+        vector<Skill>       skills;
+        int                 exp;
+        bool                alive;
 
         static long long int engimon_count;
     public:
         Engimon()
         {
+            name = "UNKNOWN";
+            exp = 0;
+            alive = false;
+        }
+
+        Engimon(int species_id) : Species(species_id)
+        {
+            id = engimon_count;
             engimon_count++;
+
+            name = getSpeciesName();
+            skills.push_back(getUniqueSkill());
+            exp = 0;
+            alive = true;
         };
+
+        Engimon(const Engimon &e)
+        {
+            id = e.id;
+            name = e.name;
+            parents = e.parents;
+            skills = e.skills;
+            exp = e.exp;
+            alive = e.alive;
+        }
+
+        Engimon& operator=(const Engimon &e)
+        {
+            Engimon *res = new Engimon(e);
+            return *res;
+        }
 
         ~Engimon()
         {
-            delete[] parents;
-            delete[] skills;
+            vector<Engimon*>().swap(parents);
+            vector<Skill>().swap(skills);
         };
 
         /* SETTERS */
@@ -108,10 +194,25 @@ class Engimon : public Engidex::Species {
 
         void PrintInfo()
         {
-            Print(name);
+            cout << "Name     : " << name << endl;
+            for(auto &i : parents) {
+            cout << "Parent   : " << i->name << endl;
+            }
+            species_info_print();
         };
 
-        void addSkill(Skill newSkill);      
+        void addSkill(Skill newSkill)
+        {
+            if (skills.size() < 4)
+            {
+                skills.push_back(newSkill);
+            } else {
+                cout << "Engimon sudah memiliki 4 skill" << endl;
+            }
+        };   
+
+        /* OPERATORS */
+        bool operator==(Engimon comp) { return id == comp.id; };
 
         /* FRIEND FUNCTIONS */
         friend Engimon breed(Engimon dad, Engimon mom); // for breeding
---
+++ src/GameInit.cpp
@@ -0,0 +1,17 @@
+#include "Engimon.hpp"
+#include "Skill.h"
+#include "Elements.hpp"
+
+// Initialize Elements
+/*
+
+*/
+
+// Initialize Skills
+
+// Initialize Pokemons
+/*
+FORMAT:
+    name, id, skill, element 1, element 2 (optional) 
+*/
+// Engidex::addSpecies("Pikachoo", d);
